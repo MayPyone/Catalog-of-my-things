05761b72bbcd26cb651fbe476279cf07260e0c8e
May Pyone <maypyone015@gmail.com>
2023-10-31 21:37:00 +0630

    Define properties and implement methods for game class

---
+++ game.rb
@@ -0,0 +1,18 @@
+require_relative 'item'
+require 'date'
+
+class Game < Item
+  def initialize(players, last_play, publish_date)
+    super(publish_date)
+    @multiplayer = players
+    @last_played_at = last_play
+  end
+
+  def last_play_difference
+    Date.parse(@last_played_at).year - Date.parse(@publish_date).year
+  end
+
+  def can_be_archived?
+    super && last_play_difference > 2
+  end
+end
