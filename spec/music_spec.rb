03ea7529603a10c5677b54dddabc2febd8305097
yusufyusie <yesuf023@gmail.com>
2023-11-04 15:16:52 +0300

    Update testing music

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -1,37 +1,21 @@
-require 'minitest/autorun'
 require_relative '../models/music_album'
 
-class MusicModuleTest < Minitest::Test
-  def setup
-    @music_module = MusicModule.new
-  end
-
-  def test_display_music_album_with_empty_list
-    assert_equal 'No music albums found', @music_module.display_music_album
-  end
-
-  def test_display_music_album_with_non_empty_list
-    @music_module.add_music_album('2023-11-04', 'y')
-    assert_equal "\nExisting music albums in the list:\n 1) Music Albem publish date: 2023-11-04 It is on spotify: true",
-                 @music_module.display_music_album
-  end
-
-  def test_add_music_album_with_valid_input
-    @music_module.add_music_album('2023-11-04', 'y')
-    assert_equal 1, @music_module.musicalbums.size
-    assert_equal '2023-11-04', @music_module.musicalbums[0].publish_date
-    assert_equal true, @music_module.musicalbums[0].on_spotify
-  end
-
-  def test_add_music_album_with_invalid_publish_date
-    assert_raises ArgumentError do
-      @music_module.add_music_album('2023-11-32', 'y')
+describe 'Testing MusicAlbum class' do
+  context 'Testing MusicAlbum instance and Method' do
+    before(:context) do
+      @musicalbum = MusicAlbum.new('2023-07-12', true)
     end
-  end
 
-  def test_add_music_album_with_invalid_on_spotify_input
-    assert_raises ArgumentError do
-      @music_module.add_music_album('2023-11-04', 'z')
+    it 'Testing on spotify instance' do
+      expect(@musicalbum.on_spotify).to be true
+    end
+
+    it 'Testing publish Date instance' do
+      expect(@musicalbum.publish_date).to eq '2023-07-12'
+    end
+
+    it 'Testing MusicAlbum class inheritance' do
+      expect(MusicAlbum).to be < Item
     end
   end
 end
